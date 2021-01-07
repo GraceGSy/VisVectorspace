513597e1565f7ef6f63d100a2b29ecb481a155ea
Grace <grace@graces-mbp.lan>
2021-01-07 12:10:41 -0500

    add loading overlay

---
+++ preprocessing/readFile.py
@@ -0,0 +1,56 @@
+import csv
+
+{'Drama': 133400,
+'Short': 12665,
+'Documentary': 42250,
+'Action': 32333,
+'Horror': 2461,
+'Adventure': 20090,
+'Comedy': 113303,
+'Crime': 21476,
+'Biography': 5247,
+'Animation': 21464,
+'Adult': 16061,
+'News': 21731,
+'Family': 23035,
+'Mystery': 1312,
+'Talk-Show': 21008,
+'Fantasy': 1500,
+'Thriller': 2069,
+'Western': 169,
+'Music': 17671,
+'Sci-Fi': 1130,
+'Game-Show': 18509,
+'History': 1256,
+'War': 178,
+'Romance': 4205,
+'Musical': 1702,
+'Sport': 7081,
+'Reality-TV': 4722}
+
+with open("title.basics.tsv") as fileMeta:
+	readMeta = csv.reader(fileMeta, delimiter="\t", quotechar='"')
+
+	# Skip headers
+	next(readMeta, None)
+
+	moviesData = ["tconst,title,type,start year,runtime (min),average rating,num votes"]
+
+	genresDict = {}
+
+	for row in readMeta:
+
+		try:
+			[tconst, titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runtimeMinutes, genres] = row
+			if (int(startYear) >= 1990) and (int(startYear) < 2000) and (genres != "\\N"):
+				genreMain = genres.split(",")[0]
+				if genreMain not in genresDict:
+					genresDict[genreMain] = 0
+
+				genresDict[genreMain] += 1
+
+
+		except Exception as e:
+			continue
+
+	print(genresDict)
