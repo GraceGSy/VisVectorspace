c4149b1d4c9a0683941b7c29e4352d2754c9aa5c
Grace <grace@graces-mbp.lan>
2021-01-09 17:07:36 -0500

    update flask

--- server.py
+++ server.py
@@ -13,14 +13,14 @@ from umap import UMAP
 app = Flask(__name__)
 
 # Path for our main Svelte page
-@app.route("/")
-def base():
-	return send_from_directory('client/public', 'index.html')
+# @app.route("/")
+# def base():
+# 	return send_from_directory('client/public', 'index.html')
 
-# Path for all the static files (compiled JS/CSS, etc.)
-@app.route("/<path:path>")
-def home(path):
-	return send_from_directory('client/public', path)
+# # Path for all the static files (compiled JS/CSS, etc.)
+# @app.route("/<path:path>")
+# def home(path):
+# 	return send_from_directory('client/public', path)
 
 # The following route is used for testing
 @app.route("/rand/<val>")
@@ -245,5 +245,5 @@ def kneighbors():
 	return result
 
 
-if __name__ == "__main__":
-	app.run(debug=True)
+# if __name__ == "__main__":
+app.run(debug=True)
